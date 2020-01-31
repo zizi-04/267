81458b7e750bddc033aaf61a591c3d809d4d19fd
zizi-04 <60290169+zizi-04@users.noreply.github.com>
2020-01-31 18:54:35 +0700

    Zizi-16919369

---
+++ main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    //Health = 0, Kalah!
+
+    printf("===     ===     ===========     ===              ============");
+    printf("\n===     ===     ===     ===     ===              ============");
+    printf("\n===     ===     ===     ===     ===              ===      ===");
+    printf("\n===========     ===========     ===              ===      ===");
+    printf("\n===========     ===========     ===              ===      ===");
+    printf("\n===     ===     ===     ===     ===              ===      ===");
+    printf("\n===     ===     ===     ===     ============     ============");
+    printf("\n===     ===     ===     ===     ============     ============");
+
+    printf("\n(selamat datang)");
+
+
+    printf("\n                   =======                      ");
+    printf("\n                 == +   + ==                    ");
+    printf("\n                 ==       ==                hilmy udah mulai dewasa dia harus bisa mandiri    ");//wasa dia harus bisa mandiri)
+    printf("\n                 == |___| ==                    ");
+    printf("\n\n                    =====                       ");
+    printf("\n                |...=====...|                   ");
+    printf("\n                    =====                        ");
+    printf("\n                    =====                       ");
+    printf("\n                    |   |                       ");
+    printf("\n                    |   |                       ");
+    printf("\n                    =   =                       ");
+
+    printf("\n             ===============================            ");
+    printf("\n             ===============================            ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |(ini tali)                       ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      |                                 ");
+    printf("\n                      =                                 ");
+    printf("\n                     ===                                ");
+    printf("\n                    =====                               ");
+    printf("\n                  =========                             ");
+    printf("\n                =============                           ");
+    printf("\n                 == x   x ==(hilmy just dead)           ");
+    printf("\n                 ==       ==                            ");
+    printf("\n                 ==   0   ==                            ");
+    printf("\n                    =====                               ");
+    printf("\n                |...=====...|                           ");
+    printf("\n                    =====                               ");
+    printf("\n                    =====                               ");
+    printf("\n                    |   |                               ");
+    printf("\n                    |   |                               ");
+    printf("\n                    =   =                               ");
+    printf("\n                                                        ");
+    printf("\n                                                        ");
+    printf("\nini tanah                                               ");
+    printf("\n=================================================       ");
+    printf("\n=================================================       \n\n");
+
+    printf("|\\      /| ------ ------- -----  \n");
+    printf("| \\    / | |    |    |      |    \n");
+    printf("|  \\  /  | |----|    |      |    \n");
+    printf("|   \\/   | |    |    |      |    \n");
+    printf("|        | |    |    |      |    \n");
+    printf("|        | |    |    |    -----  \n");
+
+    printf("|Maaf Anda telah mati!|\n\n");
+    //Credits...
+    printf("Terima kasih sudah main game kami :) \n");
+    printf("Game ini telah di buat oleh: \nMuhammad Pudga Gemilan - 16019055 \n");
+    printf("Muhammad Ilyas Irfan S. - 16519517 \n");
+    printf("Zivanka Nafisa Wongkaren - 16919369 \n\n");
+
+    //Menang!
+    printf("||        ||  ||   ||        ||  \n");
+    printf(" ||      ||  ||||   ||      || \n");
+    printf("  ||    ||  ||  ||   ||    || \n");
+    printf("   ||  ||  ||    ||   ||  ||    \n");
+    printf("     ||   ||======||    ||   \n");
+    printf("     ||   ||      ||    ||    \n");
+    printf("     ||   ||      ||    ||    \n");
+    printf("     ||   ||      ||    ||    \n");
+    printf("     ||   ||      ||    ||     \n");
+    //Money = 100
+    printf("Wah! Anda telah membangun beberapa bisnis terkenal yang cukup lebat dan makmur! \nSampai banyak yang ingin menjadi seperti Anda! \nJangan terlalu sombong ya... uangnya selalu ngalir!!!\n\n");
+    //Social = 100
+    printf("Wahh!! Hebat!! Anda telah menjadi Ketua BEM Universitas Robot Eva (URE)! \nBanyak ya teman-temannya? \nPasti paling populer!!! \nSeru ya berbakti kepada masyarakat universitas dengan jiwa yang sosial!!\n\n");
+   //Health = 100
+    printf("Selamat! Anda telah lulus kedokteran Universitas Robot Eva (URE)\ndan menjadi seorang dokter bedah yang sangat hebat! \nKesehatan benar-benar penting! \nCiee kedokterannn!!! \n\n");
+    //Hygiene = 100
+    printf("Selamat!! Anda telah menjadi seorang perawat. \nMaaf tapi Anda tidak lulus kedokteran :( \nTidak apa-apa, tidak usah beresah! \nAnda akan menjadi asisten dokter tersebut jadi... tetap jaga higenisnya ya :) \n\n");
+    //Happiness = 100
+    printf("Selamat Anda akhirnya mencapai yang cukup impossible!!! \nAkhirnya si dia membalas chatan dan kalian sudah berkencan selama 1 tahun! \nAnda datang kerumahnya untuk menanyanya 'Apakah mau menikah sama saya?' \nTetapi Anda terkejut.... ternyata si dia punya robot-cowo lain dan selama ini selingkuh :'( \nAnda jadi... forever jomblo XD \nSemangat!");
+    return 0;
+}
+
